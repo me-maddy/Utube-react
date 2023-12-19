72f1b4f9c8cf41049239603fba34d6a6d8b04833
Innocent-Maddy <103171939+me-maddy@users.noreply.github.com>
2023-12-19 20:25:55 +0530

    add Shimmer UI

---
+++ src/Components/ShimmerUI.js
@@ -0,0 +1,29 @@
+const Cart = () => {
+  return (
+    <div className="w-full bg-gray-300 rounded-lg animate-pulse h-60 md:h-72"></div>
+  );
+};
+
+const ShimmerUI = ({ type }) => {
+  return (
+    <div
+      className={
+        type
+          ? " w-full  py-1 gap-y-2.5 flex flex-wrap"
+          : "py-2.5 w-full grid gap-y-6 gap-x-4 lg:gap-x-5 mt-2.5 px-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 2xl:grid-cols-4"
+      }
+    >
+      <Cart />
+      <Cart />
+      <Cart />
+      <Cart />
+      <Cart />
+      <Cart />
+      <Cart />
+      <Cart />
+      <Cart />
+    </div>
+  );
+};
+
+export default ShimmerUI;
